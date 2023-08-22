4f137fda39a7847b9325a7aae6e3b35382839216
hafizhMo <hafizh.hizb@gmail.com>
2023-08-22 22:48:19 +0700

    Initial Commit

---
+++ Bloom Into You/Bloom_Into_YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bloom_Into_YouApp.swift
+//  Bloom Into You
+//
+//  Created by Hafizh Mo on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Bloom_Into_YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
